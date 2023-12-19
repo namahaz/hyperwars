2f5893c8e274d6334d60714adbfffefce139e4c0
Vikram Rojo <1778311+vikramrojo@users.noreply.github.com>
2023-12-18 21:08:49 -0700

    social

--- _data/metadata.js
+++ _data/metadata.js
@@ -4,8 +4,8 @@ module.exports = {
 	language: "en",
 	description: "Tracking the risks of deploying artificial intelligence in conflicts, speculating on human futures and advocating to stop hyperwars.",
 	author: {
-		name: "Stop Hyperwars",
-		email: "",
+		name: "@stophyperwars",
+		email: "info@hyperwars.org",
 		url: "https://hyperwars.org"
 	}
 }
