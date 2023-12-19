3f97f960a293271562fff797f74ab4733ac33043
Vikram Rojo <1778311+vikramrojo@users.noreply.github.com>
2023-12-18 21:40:14 -0700

    variable cover images

--- _data/metadata.js
+++ _data/metadata.js
@@ -3,6 +3,7 @@ module.exports = {
 	url: "https://hyperwars.org",
 	language: "en",
 	description: "Tracking the risks of deploying artificial intelligence in conflicts, speculating on human futures and advocating to stop hyperwars.",
+  cover: "/home.jpg",
 	author: {
 		name: "@stophyperwars",
 		email: "info@hyperwars.org",
